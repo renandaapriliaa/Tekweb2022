b439004a13788fcafcd539aff3bd71643a95f6b0
renandaapriliaa <97266037+renandaapriliaa@users.noreply.github.com>
2022-07-28 23:09:51 +0700

    Create main_final.js

---
+++ main_final.js
@@ -0,0 +1,41 @@
+let apiUrl = "https://myapi.renandaapriliaa.my.id/index.php//";
+
+Vue.createApp({
+    data() {
+      return {
+        message: "Hello Vue!",
+        header: {},
+        articles: []
+      };
+    },
+    methods:{    
+        getHeaderData()
+        {
+          axios
+            .get(apiUrl+"users/2")
+            .then((res) => {              
+              this.header = res.data;
+              this.getArticles();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+        getArticles()
+        {
+            axios
+            .get(
+             "https://raw.githubusercontent.com/renandaapriliaa/tekweb2022/main/contents/articles.json"
+            )
+            .then((res) => {              
+              this.articles = res.data;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+    },
+    beforeMount() {
+        this.getHeaderData()
+    }
+  }).mount("#app");
